0207d20a60109e477347257960333c75b0e70647
Overzet <overzet@gmail.com>
2018-07-29 22:24:35 +0200

    No global Scanner = bug is gone!?

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,12 +1,60 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Student {
     private String firstName;
     private String lastName;
-    private String gradeYear;
-    private int studentID;
+    private int gradeYear;
+    private String studentID;
     private String courses;
-    private int tuitionBalance;
-    private int costOfCourse = 600;
+    private int tuitionBalance = 0;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
+
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.print("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = in.nextInt();
+
+        setStudentID();
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+        System.out.println("***********************************");
+
+    }
+
+    private void setStudentID() {
+        id++;
+        this.studentID = gradeYear + "" + id;
+    }
+
+    public void enroll() {
+        do {
+            System.out.print("Enter course to enroll, Q to quit: ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            } else {
+                System.out.println("Break");
+                break;
+            }
+        }
+        while (1 != 0);
+
+        System.out.println("Enrolled in: " + courses);
+        System.out.println("Tuition balance is: " + tuitionBalance);
+
+
+    }
 }
+
--- src/com/company/StudentDatabaseApp.java
+++ src/com/company/StudentDatabaseApp.java
@@ -3,7 +3,11 @@ package com.company;
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello");
+        // write your code here
+        Student st1 = new Student();
+        st1.enroll();
+        //Student st2 = new Student();
+        //Student st3 = new Student();
+
     }
 }
