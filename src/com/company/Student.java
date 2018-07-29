eecdaad048659050e0deead2dabbc89aa4952a80
Overzet <overzet@gmail.com>
2018-07-29 11:48:55 +0200

    Variables added

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
 }
