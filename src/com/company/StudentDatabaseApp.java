8748f0fc0880665d53c2ac53c1cd63350679a25d
Overzet <overzet@gmail.com>
2018-07-29 11:41:18 +0200

    First commit

--- src/com/company/StudentDatabaseApp.java
+++ src/com/company/StudentDatabaseApp.java
@@ -4,6 +4,6 @@ public class StudentDatabaseApp {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println();
+        System.out.println("Hello");
     }
 }
